de214ac58526f6c15d566bbf0825a492f9ed063a
Muhammet Baskurt <67770452+mbkurt06@users.noreply.github.com>
2026-09-03 09:59:31 +0200

    Show only five focused stories

--- app/src/main/java/de/bascurt/almancaokuyucu/data/SampleLessons.kt
+++ app/src/main/java/de/bascurt/almancaokuyucu/data/SampleLessons.kt
@@ -3,11 +3,5 @@ package de.bascurt.almancaokuyucu.data
 import de.bascurt.almancaokuyucu.model.ReaderLesson
 
 object SampleLessons {
-    val all: List<ReaderLesson> by lazy {
-        ExtendedLessonsPart0.all +
-            ExtendedLessonsPart1.all +
-            ExtendedLessonsPart2.all +
-            ExtendedLessonsPart3.all +
-            ExtendedLessonsPart4.all
-    }
+    val all: List<ReaderLesson> by lazy { FocusedLessons.all }
 }
