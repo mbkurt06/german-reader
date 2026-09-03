eff0cacc8b4d4d8555fa146a62422616d07c5a0c
Muhammet Baskurt <67770452+mbkurt06@users.noreply.github.com>
2026-09-03 20:53:41 +0200

    Bump app version to 0.4.1.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "de.bascurt.almancaokuyucu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.4.1.24"
+        versionCode = 32
+        versionName = "0.4.1.25"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "CONTENT_BASE_URL", "\"https://raw.githubusercontent.com/mbkurt06/german-reader/main/content/\"")
     }
